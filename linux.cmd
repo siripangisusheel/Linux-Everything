eff8dca905beda69ba3fc7e984c561aaf76f4a9a
siripangisusheel <100437307+siripangisusheel@users.noreply.github.com>
2024-11-17 20:39:34 -0800

    Update linux.cmd

--- linux.cmd
+++ linux.cmd
@@ -105,3 +105,74 @@ cut -c2,3 file_name -To see only 2 and 4th char
 cut -d: -f 3 file_name  -To see data from CSV
 cut -d, -f 1- country.txt --output-delimiter="|" -To change the delimeter of output
 
+SED COMMANDS
+To show only a given line or range of lines
+sed -n '1p' file_name
+sed -n '1,5p' file_name
+sed -n '$p' file_name
+sed -n '/India/p' file_name -To see all the users from India Country
+To use multiple expression in sed command
+Example: If you wanna only see 2 and 5th line
+sed -n -e '2p' -e '5p' file_name
+
+sed -n -e '/India/p' -e '/Germany/p' file_name -To see all the users from India and Germany
+sed -n ‘2,+4p’ file_name -To see next 4 lines from 2nd line
+sed -n ‘1~2p’ file_name -To see every 2nd line from first line
+sed -f ex_file file_name -To read expression from external file
+sed 's/<string_to_change>/<new_string>/g' file_name -To replace a word in a file and show
+To replace a word in a file and show except a given line or only in given line?
+sed '5 s/<string_to_change>/<new_string>/g' file_name
+sed '5! s/<string_to_change>/<new_string>/g' file_name
+
+sed -i's/<string_to_change>/<new_string>/g' file_name -To replace a word and and edit in your file
+To change salary or country of a user (Paul)?
+sed '/Paul/ s/25000/35000/g' file_name
+sed '/Paul/ s/India/US/g' file_name
+
+To delete a line?
+sed '1d' file_name                                    (to delete first line)
+sed '1,2d' file_name
+sed '$d' file_name
+
+sed ‘/India/d’ file_name -To delete user from India country
+sed '/^$/d' file_name -To delete empty line
+sed 's/\t/ /g' file_name -To replace tab with space
+sed -n ‘/India/ w new_file_name’ file_name -To copy output of sed command in separate file
+sed '5 a new_text' file_name -To add new line after a given line no.
+sed '/Paul/ a new_text' file_name -To add new line after a given string, so it will add text after Paul
+sed '5 c new_text' file_name (it will add text at line 5) -To edit existing line instead of adding new line
+sed '/Paul/ i new_text' file_name -To add new line before a given string, so it will add text before Paul
+sed -n 'l' file_name -To see the hidden characters
+sed -n 'l 50' file_name -To wrap your file content with given no. of characters
+sed '3 r externalfile' file_name -To read content from a file and use in our command
+To stop execution of sed command as soon as first occurance found
+sed ‘/India/ q’ file_name
+sed ‘5 q’ file_name  (stop execution at line 5)
+sed ‘/India/ q 100’ file_name -To provide exit status for your sed command
+sed '2 e date' file_name -To execute external command line date in your expression
+sed '=' file_name -To see the line number in file
+
+SED Regular Expressions
+
+^   start of line
+$   end of line
+.   single character
+[]  match character set
+[^] exclusive Set 
+*   zero or more occurance
+
+examples:
+
+sed -n '/^2/p' file_name
+sed -n '/ia$/p' file_name
+
+sed -n '/^S...a$/p' names -To find a 5 letter name which start with S and end with a
+sed -n '/^V/p' names -To find names start with V
+sed -n '/a$/p' names -To find names end with a
+ls -ltr *.txt -To use wild card
+To see names start with only A and C
+sed -n '/[AC]/p' names
+sed -n '/[AC]/p' names
+sed -n '/^[A-D]/p' names -To see names start with only A to D
+
+
